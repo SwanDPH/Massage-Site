bb3779f173503d1285dd363a1d87c069cc7e18bf
SwanDPH <swan.daphne@gmail.com>
2023-03-08 19:46:08 +0100

    first transfer

---
+++ massage.js
@@ -0,0 +1,12 @@
+let iconeResponsive = document.querySelector('.iconResponsive')
+let DIVmenuResponsive = document.querySelector('.menuResponsive')
+let ULmenuResponsive = document.querySelector('.responsive')
+
+
+iconeResponsive.addEventListener('click', () => {
+    DIVmenuResponsive.classList.toggle('visible');
+    DIVmenuResponsive.classList.toggle('responsiveVisible');
+})
+
+
+
